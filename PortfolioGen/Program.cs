f5cc29376830dfe073bb96115313a7c502e4889a
Hoomam <hoaz1600@student.miun.se>
2025-06-07 10:00:40 +0200

    Fixed nullable warnings i GithubClient

--- PortfolioGen/Program.cs
+++ PortfolioGen/Program.cs
@@ -24,40 +24,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication()
     .AddGitHub(options =>
     {
-        options.ClientId = builder.Configuration["Authentication:GitHub:ClientId"];
-        options.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"];
+        options.ClientId = builder.Configuration["Authentication:GitHub:ClientId"] 
+        ?? throw new InvalidOperationException("Github client id is missing");
+        options.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"]
+        ?? throw new InvalidOperationException("Github ClientSecret is missing");
         options.ClaimActions.MapJsonKey("urn:github:login", "login");
         options.ClaimActions.MapJsonKey("urn:github:name", "name");
         options.Scope.Add("public_repo");
         options.SaveTokens = true;
-
-        /*options.Events = new OAuthEvents
-        {
-            OnCreatingTicket = async context =>
-            {
-                
-                var githubUsername = context.Identity.FindFirst("urn:github:login")?.Value;
-                var displayName = context.Identity.FindFirst("urn:github:name")?.Value;
-
-                var userManager = context.HttpContext.RequestServices.GetRequiredService<UserManager<AppUser>>();
-
-                var loginProvider = context.Options.ClaimsIssuer;
-                var providerKey = context.Principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-                var user = await userManager.FindByLoginAsync(loginProvider, providerKey);
-
-                if (user != null)
-                {
-                    if (string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.Name))
-                    {
-                        user.UserName ??= githubUsername;
-                        user.Name ??= displayName;
-                        await userManager.UpdateAsync(user);
-                    }
-                }
-            }
-        };*/
-
     });
 
 builder.Services.AddHttpClient(); // HttpClient
